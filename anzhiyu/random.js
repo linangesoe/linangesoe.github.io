ef322b90cb3012ac145e879aa434293cd42b5e30
linangesoe <740080298@qq.com>
2023-12-27 14:33:28 +0800

    Site updated: 2023-12-27 14:33:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/12/24/hello-world/","2023/12/24/测试/"];function toRandomPost(){
+var posts=["2023/12/24/测试/","2023/12/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
